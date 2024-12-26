431159354fe1c096b1eb56245484616ca8f531b1
urielbang <uri32655@gmail.com>
2024-12-26 14:58:17 +0200

    fix logic

--- javascript/favorites.js
+++ javascript/favorites.js
@@ -1,18 +1,20 @@
 const favoritesLS = JSON.parse(localStorage.getItem("favorites"));
 
 favoritesLS.forEach((elemet, index) => {
-  let date = new Date(elemet.release_date).getFullYear();
+  if (elemet) {
+    let date = new Date(elemet.release_date).getFullYear();
 
-  document.querySelector(
-    "#mainPopular"
-  ).innerHTML += `<div style="display: flex;flex-direction: column;" class="card" id="card${index}">
-  <img  src="https://image.tmdb.org/t/p/w500/${elemet.poster_path}">
-  <div style="display: flex;flex-direction: row;" class="divHeart"> 
-  <p> ${elemet.original_title}</p>
-  <i class="fa fa-heart" style="font-size: 20px"></i>
-   </div>
-  <span>${date}</span>
-  </div>`;
+    document.querySelector(
+      "#mainPopular"
+    ).innerHTML += `<div style="display: flex;flex-direction: column;" class="card" id="card${index}">
+    <img  src="https://image.tmdb.org/t/p/w500/${elemet.poster_path}">
+    <div style="display: flex;flex-direction: row;" class="divHeart"> 
+    <p> ${elemet.original_title}</p>
+    <i class="fa fa-heart" style="font-size: 20px"></i>
+     </div>
+    <span>${date}</span>
+    </div>`;
+  }
 });
 
 //!navBar
