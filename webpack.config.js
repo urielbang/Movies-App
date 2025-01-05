3ac6d5ab84d7e8c62f35b9b787b245cf810e3ef6
urielbang <uri32655@gmail.com>
2025-01-05 12:47:13 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,36 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true, // Clean the output directory before each build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Handle CSS files
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/, // Handle image files
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // Path to your index.html
+      filename: "index.html", // Name of the output file
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"), // Serve static files from 'dist'
+    },
+    compress: true,
+    port: 9000, // Local development server port
   },
 };
