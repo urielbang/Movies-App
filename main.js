d92a77c1bd2eed2beeefb3b949a441dfca40e8d3
urielbang <uri32655@gmail.com>
2025-09-09 10:58:31 +0300

    update likes

--- main.js
+++ main.js
@@ -31,51 +31,50 @@ const fetchDefaultData = async (pageNumber) => {
   }
 };
 
-// fetch(
-//   "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-//   options
-// )
-//   .then((response) => response.json())
-//   .then((response) => {
-//     response.results.forEach((element, index) => {
-//       let date = new Date(element.release_date).getFullYear();
+fetch(
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    response.results.forEach((element, index) => {
+      let date = new Date(element.release_date).getFullYear();
 
-const getMovies = async () => {
-  const movies = await fetchDefaultData(1);
-  console.log(movies);
-};
+      const getMovies = async (pageNumber) => {
+        const movies = await fetchDefaultData(pageNumber);
+      };
 
-getMovies();
+      getMovies(1);
 
-//       //!slider
-//       document.querySelector("#slider").innerHTML += `
+      //       //!slider
+      document.querySelector("#slider").innerHTML += `
 
-//       <div style="display: flex;flex-direction: column;" class="cardSlider">
-//       <img  src="https://image.tmdb.org/t/p/w500/${element.poster_path}">
-//       <p> ${element.original_title}</p>
-//       <span>${date}</span>
-//       <div>
+            <div style="display: flex;flex-direction: column;" class="cardSlider">
+            <img  src="https://image.tmdb.org/t/p/w500/${element.poster_path}">
+            <p> ${element.original_title}</p>
+            <span>${date}</span>
+            <div>
 
-//       `;
-//       //!the all page!
-//       document.querySelector("#mainPopular").innerHTML += `
-//       <div style="display: flex;flex-direction: column;" class="card" id="card${index}">
-//       <img  src="https://image.tmdb.org/t/p/w500/${element.poster_path}">
-//       <div style="display: flex;flex-direction: row;" class="divHeart">
-//       <p> ${element.original_title}</p>
-//       <i class="fa fa-heart" style="font-size: 20px"></i>
-//        </div>
-//       <span>${date}</span>
-//       </div>
-//       `;
-//     });
-//   })
-//   .catch((err) => console.error(err));
+            `;
+      //       //!the all page!
+      document.querySelector("#mainPopular").innerHTML += `
+      <div style="display: flex;flex-direction: column;" class="card" id="card${index}">
+      <img  src="https://image.tmdb.org/t/p/w500/${element.poster_path}">
+      <div style="display: flex;flex-direction: row;" class="divHeart">
+      <p> ${element.original_title}</p>
+      <i class="fa fa-heart" style="font-size: 20px"></i>
+       </div>
+      <span>${date}</span>
+      </div>
+      `;
+    });
+  })
+  .catch((err) => console.error(err));
 
 //!hearts
 
 const handleLikes = async () => {
-  const data = await fetchDefaultData();
+  const data = await fetchDefaultData(1);
 
   hearts = document.querySelectorAll(".fa-heart");
 
@@ -86,7 +85,7 @@ const handleLikes = async () => {
       document.querySelector(
         "#movieClickDisplay"
       ).innerHTML = `<div class="cardOnDisplay">${cardClass[i].innerHTML}</div>
-       <p>${data.results[i].overview}</p>
+       <p>${data[i].overview}</p>
        
        `;
     });
@@ -94,7 +93,7 @@ const handleLikes = async () => {
 
   const onClickHeart = (heart, index) => {
     heart.style.color = "red";
-    const movieLiked = data.results[index];
+    const movieLiked = data[index];
 
     if (
       !likesLocalStorage.some(
@@ -120,7 +119,7 @@ const handleLikes = async () => {
 
   if (likesLocalStorage.length > 0) {
     let indicesLiked = likesLocalStorage.map((movieLiked) => {
-      return data.results.findIndex(
+      return data.findIndex(
         (movie) => movie.original_title === movieLiked.original_title
       );
     });
